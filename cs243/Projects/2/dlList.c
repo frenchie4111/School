9bc47e7e8e68609d8cb5b17200da6e9488f86e11
Mike Lyons <mdl0394@gmail.com>
2014-04-03 10:49:51 -0400

    Updated makefile, added template code to testdllis

--- cs243/Projects/2/dlList.c
+++ cs243/Projects/2/dlList.c
@@ -1,6 +1,57 @@
+/**
+ * File: <<TEMPLATE MARK>>
+ * <<TEMPLATE MARK>>
+ * @author mdl7240 : Mike Lyons
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    void *prev;
+    void *next;
+    void *data;
+} * DlList_T;
+
 #define _DLL_IMPL_
 #include "dlList.h"
 
-typedef struct DlList_T {
+DlList_T dll_create( void ) {
+    DlList_T new_node = (DlList_T)malloc( sizeof( DlList_T ) );
+    
+    if( new_node != NULL ) {
+        new_node->prev = NULL;
+        new_node->next = NULL;
+        new_node->data = NULL;
+    }
 
-} DlList_T;
+    return new_node;
+}
+
+void dll_destroy( DlList_T lst ) {
+    while( lst ) {
+        DlList_T next = lst->next;
+
+        free( lst->data );
+        free( lst );
+
+        lst = next;
+    }
+}
+
+void dll_clear( DlList_T lst ) {    
+    free( lst->data );
+    dll_destroy( lst->next ); // Just don't free the first one
+}
+
+int dll_size( DlList_T lst ) {
+    int counter = 0;
+    while( lst ) {
+        lst = lst->next;
+        counter++;
+    }
+    return counter;
+}
+
+void dll_append( DlList_T lst, void *data ) {
+
+}
---
+++ cs243/Projects/2/testdllist.c
@@ -0,0 +1,30 @@
+/**
+ * File: <<TEMPLATE MARK>>
+ * <<TEMPLATE MARK>>
+ * @author mdl7240 : Mike Lyons
+ */
+#include <stdio.h>
+#include <string.h>
+
+#include "dlList.h"
+
+/**
+ * Main function
+ *
+ * (a) Tests insert and delete operations.
+ * (b) Tests move forward beyond last element.
+ * (c) Tests move back beyond ﬁrst element.
+ * (d) Tests cursor absolute and relative positioning.
+ * (e) Tests cursor state
+ * 
+ * @param argc the number of arguments
+ * @param argv array of arguments
+ * @returns int the error code; 0 if no error
+ */
+int main( int argc, const char* argv[] ) {
+    DlList_T test = dll_create();
+
+    
+
+    dll_destroy( test );
+}
